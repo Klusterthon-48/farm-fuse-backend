d0f3398d98a23c033bdf9225534db02223108811
Adeolu Amole <adeoluamole@gmail.com>
2023-11-21 11:14:26 +0100

    Created routes, model and controllers folder

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
-const PORT = process.env.PORT || 2500;
+const PORT = process.env.PORT || 5500;
 const mongoose_URI = process.env.URI;
 
 const cors = require("cors");
