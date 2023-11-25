60d0588b2ba926785047e8d95df39d12b9c67272
Adeolu Amole <adeoluamole@gmail.com>
2023-11-25 20:08:51 +0100

    feat: Style the reset email

--- src/utils/libs/emailUtils.js
+++ src/utils/libs/emailUtils.js
@@ -9,9 +9,11 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+
+const farmFuseLogo =
+"https://res.cloudinary.com/dn4gfzlhq/image/upload/v1700938194/farm_d14gwq.png";
+
 export const sendWelcomeEmail = async (email, name) => {
-  const farmFuseLogo =
-    "https://res.cloudinary.com/dn4gfzlhq/image/upload/v1700938194/farm_d14gwq.png";
 
   const mailOptions = {
     from: process.env.USER,
@@ -34,6 +36,7 @@ export const sendWelcomeEmail = async (email, name) => {
 };
 
 
+
 export const sendPasswordResetEmail = async (email, name, resetToken) => {
   const resetLink = `${process.env.CLIENT_URL}/api/reset_password?email=${email}&token=${resetToken}`;
 
@@ -42,13 +45,16 @@ export const sendPasswordResetEmail = async (email, name, resetToken) => {
     to: email,
     subject: "Password Reset",
     html: `
+    <div style="background-color: #307C31; padding: 20px; text-align: center; color: #ffffff;">
+        <img src="${farmFuseLogo}" alt="Farm Fuse Logo" style="max-width: 150px; height: 40px; margin-bottom: 20px;">
             <p>Hello, ${name}</p>
             <p>You are receiving this email because you (or someone else) have requested the reset of the password for your account.</p>
             <p>Please click on the following link to reset your password:</p>
-            <a href="${resetLink}" target="_blank" rel="noopener noreferrer">Reset Password</a>
+            <a href="${resetLink}" target="_blank" rel="noopener noreferrer" style="color: #4DA1FF; text-decoration: underline;">Reset Password</a>
             <p>If you did not request this, please ignore this email, and your password will remain unchanged.</p>
             <p>Thank you,</p>
             <p>The Farm Fuse Team</p>
+      </div>
           `,
   };
 
