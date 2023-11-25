25d2a811dc69ee8584de7a4db0d0fa3a50039971
aniebietafia <aniebietafia87@gmail.com>
2023-11-25 22:30:36 +0100

    updated register handler

--- src/controllers/farmer_account.controller.js
+++ src/controllers/farmer_account.controller.js
@@ -6,7 +6,6 @@ import { hashPassword, comparePasswords } from "../utils/libs/bcryptUtils.js";
 import { generateResetToken, signToken, verifyToken } from "../utils/libs/jwtUtils.js";
 import { sendPasswordResetEmail, sendWelcomeEmail } from "../utils/libs/emailUtils.js";
 
-
 // Function to create a new farmer
 export const createNewFarmer = tryCatchLibs(async (req, res) => {
   const { name, email, password } = req.body;
@@ -22,24 +21,19 @@ export const createNewFarmer = tryCatchLibs(async (req, res) => {
   // Create a new farmer with the hashed password
   await farmerModel.create({ name, email, password: hashedPassword });
 
- // Send a welcome email to the newly registered farmer
- try {
-  await sendWelcomeEmail(email, name);
-  console.log('Welcome email sent successfully.');
-} catch (error) {
-  console.error('Error sending welcome email:', error);
-}
+  // Send a welcome email to the newly registered farmer
+  //  try {
+  //   await sendWelcomeEmail(email, name);
+  //   console.log('Welcome email sent successfully.');
+  // } catch (error) {
+  //   console.error('Error sending welcome email:', error);
+  // }
 
-return successResponse(res, "Farmer created", StatusCodes.CREATED);
+  return successResponse(res, "Farmer created", StatusCodes.CREATED);
 });
 
-
-
-
-
-
 export const authenticateFarmer = tryCatchLibs(async (req, res) => {
-  const {email, password } = req.body;
+  const { email, password } = req.body;
 
   const farmer = await farmerModel.findOne({ email });
   if (!farmer) {
@@ -60,10 +54,6 @@ export const authenticateFarmer = tryCatchLibs(async (req, res) => {
   // Send success response with the token and farmer details
   return successResponse(res, "Authentication successful", { farmer, token }, StatusCodes.OK);
 });
-
-
-
-
 
 export const accessDashboard = tryCatchLibs(async (req, res) => {
   const token = req.headers.authorization;
@@ -86,11 +76,6 @@ export const accessDashboard = tryCatchLibs(async (req, res) => {
 
   return successResponse(res, "Access granted", dataFromDatabase, StatusCodes.OK);
 });
-
-
-
-
-
 
 export const forgotPassword = tryCatchLibs(async (req, res) => {
   const { email } = req.body;
@@ -122,9 +107,6 @@ export const forgotPassword = tryCatchLibs(async (req, res) => {
 
   return successResponse(res, "Password reset email sent successfully", null, StatusCodes.OK);
 });
-
-
-
 
 export const resetPassword = tryCatchLibs(async (req, res) => {
   const { resetPasswordToken, email, newPassword } = req.body;
@@ -168,4 +150,3 @@ export const resetPassword = tryCatchLibs(async (req, res) => {
 
   return successResponse(res, "Password reset successful. Login with your new Password", null, StatusCodes.OK);
 });
-
