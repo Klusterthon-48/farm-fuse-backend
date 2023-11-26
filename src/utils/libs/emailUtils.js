cfd7e447aac067f56e0256dd13eefcb1833a622c
Adeolu Amole <adeoluamole@gmail.com>
2023-11-26 18:54:29 +0100

    Update emailUtils.js with reset link fix

--- src/utils/libs/emailUtils.js
+++ src/utils/libs/emailUtils.js
@@ -39,7 +39,8 @@ export const sendWelcomeEmail = async (email, name) => {
 
 
 export const sendPasswordResetEmail = async (email, name, resetToken) => {
-  const resetLink = `https://farm-fuse-frontend.vercel.app/api/reset_password?email=${email}&token=${resetToken}`;
+  const resetLink = `https://farm-fuse-frontend.vercel.app/api/reset_password?email=${email}&resetPasswordToken=${resetToken}`;
+
 
   const mailOptions = {
     from: process.env.USER,
